9d01170b57411943793fd9c395d1fabd718cf593
Chris Hamons <chris.hamons@xamarin.com>
2017-07-13 10:19:56 -0500

    AppKit b3 changes

--- src/appkit.cs
+++ src/appkit.cs
@@ -9385,6 +9385,10 @@ namespace XamCore.AppKit {
 		[Mac (10, 12, 2)]
 		[Field ("NSImageNameTouchBarVolumeUpTemplate")]
 		TouchBarVolumeUpTemplate,
+
+		[Mac (10, 13)]
+		[Field ("NSImageNameTouchBarRemoveTemplate")]
+		TouchBarRemoveTemplate
 	}
 
 	interface NSStringAttributes {
@@ -10054,6 +10058,10 @@ namespace XamCore.AppKit {
 		[Mac (10, 13)]
 		[NullAllowed, Export ("ratingPlaceholderImage", ArgumentSemantic.Strong)]
 		NSImage RatingPlaceholderImage { get; set; }
+
+		[Mac (10, 13)]
+		[Export ("editable")]
+		bool Editable { [Bind ("isEditable")] get; set; }
 	}
 
 	[BaseType (typeof (NSActionCell))]
@@ -11328,6 +11336,14 @@ namespace XamCore.AppKit {
 		[Mac (10,6)]
 		[Field ("NSPasteboardTypeFindPanelSearchOptions")]
 		NSString NSPasteboardTypeFindPanelSearchOptions { get; }
+
+		[Mac (10, 13)]
+		[Field ("NSPasteboardTypeURL")]
+		NSString NSPasteboardTypeURL { get; }
+
+		[Mac (10, 13)]
+		[Field ("NSPasteboardTypeFileURL")]
+		NSString NSPasteboardTypeFileURL { get; }
 
 		[Mac (10,12)]
 		[Export ("prepareForNewContentsWithOptions:")]
